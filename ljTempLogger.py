31b6f1b5ef78eec6508311a9d1c481a7553a9491
Mick Phillips <mick.phillips@gmail.com>
2015-06-30 12:06:28 +0100

    Corrected cold-j compensation; added comments.

--- ljTempLogger.py
+++ ljTempLogger.py
@@ -1,22 +1,34 @@
 import u6
 from ktypeExample import mVoltsToTempC, tempCToMVolts
 
-CHANNEL = 0
+# AIN1 screw terminal is physically closest to internal T-sensor.
+CHANNEL = 1
+# Cold junction offset measured with thermapen: T_measured - T_internal.
+CJOFFSET = 1.4
+# Kelvin to DegC offset.
+KCOFFSET = 273.15
 
 def main():
+    # The U6 device.
     d = u6.U6()
+    # Fetch calibration data and configure d.
     d.getCalibrationData()
 
     def readTemperature():
-        coldJunc_C = d.getTemperature() + 2.5 - 273.15  
+        # Cold junction in degC, with sensor-terminal offset compensation.
+        coldJunc_C = d.getTemperature() + CJOFFSET - KCOFFSET
+        # Cold junction im mV (from inverse k-type polynomial).
         coldJunc_mV = tempCToMVolts(coldJunc_C)
-        couple_mV = d.getAIN(0, resolutionIndex=8, gainIndex=3) * 1000.
+        # Remote junction mV.
+        couple_mV = d.getAIN(CHANNEL, resolutionIndex=8, gainIndex=3) * 1000.
+        # Cold junction + remote junction mV.
         total_mV = coldJunc_mV + couple_mV
+        # Return 0-referenced temperature (from k-type polynomial).
         return mVoltsToTempC(total_mV)
     
 
     while True:
         print readTemperature()
 
-if __name__ == main():
+if __name__ == '__main__':
     main()
